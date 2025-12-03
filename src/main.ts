611dfe4c6633fbca2393e39ae7081b9d2602e84e
murilo mendo vidi <murilomendo3@gmail.com>
2025-12-02 21:56:03 -0300

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Aplicação rodando na porta ${port}`);
   console.log(`Documentação disponível em http://localhost:${port}/docs`);
 }
